b6ff79c072eb4ba157c2c14679521524f28f3bc8
dawoodkaleem <dawood9743@gmail.com>
2024-08-28 19:54:16 +0500

    new File Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
-
-
+import { InputBox } from "./components"
+import { useState } from "react"
+import useCurrencyInfo from './assets/hooks/useCurrencyInfo'
 function App() {
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
 
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
 
   return (
+    
     <>
        <h1 className="text-3xl font-bold underline">
       Hello world!
